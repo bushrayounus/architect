7f7d95b35436f4b32153ea68f4eb5d698a94c0de
Bushra Younus <41996627+bushrayounus@users.noreply.github.com>
2026-07-10 01:40:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+const projects = {
 
+project1:{
+title:"Modern Residence",
+type:"Residential",
+images:3
+},
+
+project2:{
+title:"Office Interior",
+type:"Commercial",
+images:5
+},
+
+project3:{
+title:"Villa",
+type:"Residential",
+images:8
+}
+
+};
 console.log("Bushra Younus Website Loaded");
